f4b1217056101c0f7988c943b72cb5b33073803d
Yann Darwish <yann.darwish@gmail.com>
2022-11-17 17:08:48 +0100

    employee fetch

--- src/pages/EmployeeList/EmployeeList.jsx
+++ src/pages/EmployeeList/EmployeeList.jsx
@@ -1,4 +1,23 @@
+import { useState, useEffect } from "react"
+
 const EmployeeList = () => {
+	const [employees, setEmployees] = useState([]) 
+
+	useEffect(() => {
+		const fetchEmployees = async () => {
+			try {
+				const response = await fetch("http://localhost:3000/employees")
+				const data = await response.json()
+				setEmployees(data)
+			} catch (error) {
+				console.error(error)
+			}
+		}
+		fetchEmployees()
+	}, [])
+
+	console.log(employees)
+
 	return (
 		<>
 			<h1 className="main-title">Employee List</h1>
