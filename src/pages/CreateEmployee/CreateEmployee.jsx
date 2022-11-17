1dc020ed84fa3942b436c79aedce01b41c679f5b
Yann Darwish <yann.darwish@gmail.com>
2022-11-17 16:11:04 +0100

    create employee

--- src/pages/CreateEmployee/CreateEmployee.jsx
+++ src/pages/CreateEmployee/CreateEmployee.jsx
@@ -1,25 +1,31 @@
 import Input from "../../components/Input/Input"
+import SelectInput from "../../components/SelectInput/SelectInput"
+import Button from "../../components/Button/Button"
 import "./CreateEmployee.css"
+
+const states = ["alabama", "arkansas", "algeria", "argentina"]
 
 const CreateEmployee = () => {
 // todo add onChange function to update redux state when input is changed
 	return (
 		<>
 			<h1 className="main-title">Create Employee</h1>
-			<form class="form">
-				<Input label="First Name"/>
+			<form className="form">
+				<Input label="First Name" />
 				<Input label="Last Name" />
-				<Input type="date" label="Date of Birth"/>
+				<Input type="date" label="Date of Birth" />
 				<Input type="date" label="Start Date" />
 				<fieldset className="fieldset">
 					<legend>Address</legend>
 					<Input label="Street" />
 					<Input label="City" />
 					{/* select input */}
+					<SelectInput label="State" data={states} />
 					<Input label="Zip Code" />
 				</fieldset>
 				{/* select input */}
-				<button>Save</button>
+				<SelectInput label="Department" data={states} />
+				<Button label="Save" />
 			</form>
 		</>
 	)
