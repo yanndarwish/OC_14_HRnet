6d86e907585099984306599b87e51a53fb280f06
Yann Darwish <yann.darwish@gmail.com>
2022-11-17 12:36:46 +0100

    import update

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom"
 import Root from "../components/Root/Root"
-import CreateEmployee from "../pages/CreateEmployee"
-import EmployeeList from "../pages/EmployeeList"
+import CreateEmployee from "../pages/CreateEmployee/CreateEmployee"
+import EmployeeList from "../pages/EmployeeList/EmployeeList"
 
 const router = createBrowserRouter([
 	{
