4308bd755c4b0761ed7dc37f3e65532e50b682ed
Yann Darwish <yann.darwish@gmail.com>
2022-11-19 14:08:00 +0100

    pager

---
+++ src/components/TableComponents/Pager/Pager.jsx
@@ -0,0 +1,41 @@
+import "./Pager.css"
+import { updateCurrentPage } from "../../../redux/features/tableSlice"
+import { useDispatch, useSelector } from "react-redux"
+
+
+const Pager = ({ pageAmount }) => {
+	const dispatch = useDispatch()
+	const currentPage = useSelector(state => state.table.currentPage)
+
+	const handleClick = (e) => {
+		if (e.target.value === "next") {
+			dispatch(updateCurrentPage(currentPage + 1))
+		} else {
+			dispatch(updateCurrentPage(currentPage - 1))
+		}
+	}
+
+	return (
+		<div className="pager">
+			<button
+				className={currentPage === 1 ? "disabled page-button" : "page-button"}
+				value="prev"
+				onClick={(e) => handleClick(e)}
+			>
+				Previous
+			</button>
+			<span>{currentPage}</span>
+			<button
+				className={
+					currentPage === pageAmount ? "disabled page-button" : "page-button"
+				}
+				value="next"
+				onClick={(e) => handleClick(e)}
+			>
+				Next
+			</button>
+		</div>
+	)
+}
+
+export default Pager
