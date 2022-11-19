ec2374050bea85f5196d74344187d6922540b3d9
Yann Darwish <yann.darwish@gmail.com>
2022-11-19 14:07:18 +0100

    table row

---
+++ src/components/TableComponents/TableRow/TableRow.jsx
@@ -0,0 +1,19 @@
+import "../TableHeader/TableHeader.css"
+
+const TableRow = ({ employee }) => {
+	return (
+		<tr className="table-row" id={employee.id}>
+			<td className="table-cell">{employee.firstName}</td>
+			<td className="table-cell">{employee.lastName}</td>
+			<td className="table-cell">{employee.startDate}</td>
+			<td className="table-cell">{employee.department}</td>
+			<td className="table-cell">{employee.dateOfBirth}</td>
+			<td className="table-cell">{employee.street}</td>
+			<td className="table-cell">{employee.city}</td>
+			<td className="table-cell">{employee.state}</td>
+			<td className="table-cell">{employee.zipCode}</td>
+		</tr>
+	)
+}
+
+export default TableRow
